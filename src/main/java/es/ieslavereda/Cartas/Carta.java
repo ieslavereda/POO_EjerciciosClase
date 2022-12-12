e652195e94442c3fcba25a9b4ad6f7bfcd15a327
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2022-12-12 15:14:26 +0100

    Inicial commit

---
+++ src/main/java/es/ieslavereda/Cartas/Carta.java
@@ -0,0 +1,13 @@
+package es.ieslavereda.Cartas;
+
+public class Carta {
+
+    private String palo;
+    private int numero;
+
+    public Carta(String palo, int numero){
+        this.palo = palo;
+        this.numero = numero;
+    }
+    
+}
