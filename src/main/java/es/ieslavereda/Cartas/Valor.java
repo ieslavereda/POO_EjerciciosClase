6404f158b61f8be59866803283f32404e63b2f37
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2022-12-21 19:47:50 +0100

    Inicial commit

--- src/main/java/es/ieslavereda/Cartas/Valor.java
+++ src/main/java/es/ieslavereda/Cartas/Valor.java
@@ -1,7 +1,7 @@
 package es.ieslavereda.Cartas;
 
 public enum Valor {
-    UNO("A",new int[]{1,11}),
+    A("A",new int[]{1,11}),
     DOS("2",new int[]{2}),
     TRES("3",new int[]{3}),
     CUATRO("4",new int[]{4}),
@@ -10,11 +10,11 @@ public enum Valor {
     SIETE("7",new int[]{7}),
     OCHO("8",new int[]{8}),
     NUEVE("9",new int[]{9}),
-    DIEZ("J",new int[]{10}),
-    ONCE("Q",new int[]{10}),
-    DOCE("K",new int[]{10});
-    private String shape;
-    private int[] puntuacion;
+    J("J",new int[]{10}),
+    Q("Q",new int[]{10}),
+    K("K",new int[]{10});
+    private final String shape;
+    private final int[] puntuacion;
 
     Valor(String shape,int[] puntuacion){
         this.shape=shape;
--- src/test/java/JugadorTest.java
+++ src/test/java/JugadorTest.java
@@ -2,79 +2,95 @@ import es.ieslavereda.Cartas.Carta;
 import es.ieslavereda.Cartas.Jugador;
 import es.ieslavereda.Cartas.Palo;
 import es.ieslavereda.Cartas.Valor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Testeo de obtencion de la puntuacion de un Jugador")
 public class JugadorTest {
 
     private Jugador jugador;
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("**--- Ejecutando los test ---**");
+    }
+
     @BeforeEach
     void init(){
         jugador = new Jugador("Test");
     }
+    @DisplayName("Comprobando 3A, 2, 5, y Q")
     @Test
     void check_score_3A25Q(){
 
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
         jugador.addCard(new Carta(Valor.CINCO,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
         jugador.addCard(new Carta(Valor.DOS,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
 
         assertEquals(jugador.getPuntuacion(),20);
     }
+
+    @DisplayName("Test para comprobar si se pasa")
     @Test
     void check_score_JJQ(){
 
-        jugador.addCard(new Carta(Valor.DIEZ,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.DIEZ,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.ONCE,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.J,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.J,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.Q,Palo.CLUB));
 
         assertEquals(jugador.getPuntuacion(),-1);
     }
+
+    @DisplayName("Comprobando 3A, J")
     @Test
     void check_score_3A10(){
 
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.DIEZ,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.J,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
 
         assertEquals(jugador.getPuntuacion(),13);
     }
+    @DisplayName("Comprobando 2A, J")
     @Test
     void check_score_2A10(){
 
-        jugador.addCard(new Carta(Valor.DIEZ,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.J,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
 
         assertEquals(jugador.getPuntuacion(),12);
     }
 
+    @DisplayName("Comprobando 4A, 6")
     @Test
     void check_score_4A6(){
 
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
         jugador.addCard(new Carta(Valor.SEIS,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
 
         assertEquals(jugador.getPuntuacion(),20);
     }
+    @DisplayName("Comprobando 2A")
     @Test
     void check_score_2A(){
 
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
-        jugador.addCard(new Carta(Valor.UNO,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
+        jugador.addCard(new Carta(Valor.A,Palo.CLUB));
 
-        assertEquals(jugador.getPuntuacion(),12);
+        assertEquals(12, jugador.getPuntuacion());
     }
 
+    @AfterAll
+    static void afterAll() {
+        System.out.println("**---   Test finalizado   ---**");
+    }
 }
